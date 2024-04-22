e206c0727b4ecfe303fd58d3ff1cc293e3ee2546
Mel Jones <121836871+MelJ03@users.noreply.github.com>
2024-04-22 18:44:41 -0500

    Add files via upload

---
+++ P10_Melvin/P10_Melvin/dbo.Table.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[Employee]
+(
+	[Employee ID] INT NOT NULL PRIMARY KEY CLUSTERED IDENTITY(1, 1), 
+    [Name] NCHAR(50) NOT NULL, 
+    [Position] NCHAR(50) NOT NULL, 
+    [Hourly Pay Rate] MONEY NOT NULL
+)
